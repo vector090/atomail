78c7127d13f44bd2a098424eb8c6558515c8bd3e
rootuc <rootuc@notunixchan>
2025-09-29 07:17:40 +0000

    update

---
+++ run-imap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# AtoMail script to convert POP3 emails to Atom feed
+# Usage: ./run.sh
+
+python3 atomail.py \
+  --mode imap \
+  --host v90.pp.ua \
+  --port 50143 \
+  --user u1@local.nuc \
+  --password 123456 \
+  --uri http://example.com/feed.xml \
+  --title "My POP3 Feed" \
+  -d \
+  imap.xml
+
+exit
